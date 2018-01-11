ecf94e874006ef4fce438ba2444b17edca3e2f28
Danny <contact@danfoster.io>
2018-01-11 10:43:27 -0600

    Math Dojo start

---
+++ Math Dojo/math_dojo.py
@@ -0,0 +1,11 @@
+class MathDojo(object):
+    def __init__(self):
+        pass
+    def add(self, number):
+        return self
+    def subtract(self, number):
+        pass
+        return self
+
+md = MathDojo()
+
